39ff8f24f74f5636a25865a131c3df0dfab22650
santosjackiee <jacksantos1408@gmail.com>
2022-11-24 19:22:22 -0300

    feat: controller create user

--- src/controllers/auth.controllers.js
+++ src/controllers/auth.controllers.js
@@ -1,7 +1,17 @@
-export async function signUp (req, res) {
+import { sessions, users } from "../database/db.js";
+import bcrypt from "bcrypt";
+import { v4 as uuidV4 } from "uuid";
 
+export async function signUp(req, res) {
+  const user = res.locals.user;
+  const passwordHash = bcrypt.hashSync(user.password, 10);
+
+  try {
+    await users.insertOne({ ...user, password: passwordHash });
+    res.sendStatus(201);
+  } catch (err) {
+    console.log(err);
+    res.sendStatus(500);
+  }
 }
 
-export async function signIn (req, res){
-
-}
