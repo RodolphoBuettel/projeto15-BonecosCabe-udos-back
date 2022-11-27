337544c7fe2235f702d2e9014b86db332568e712
santosjackiee <jacksantos1408@gmail.com>
2022-11-27 16:23:46 -0300

    fix: cart bugs

--- src/controllers/cartController.js
+++ src/controllers/cartController.js
@@ -15,12 +15,13 @@ export async function createCart(req, res) {
 export async function findCart(req, res) {
   const user = res.locals.user;
 
+  console.log("User do findcart: "+user);
+
   try {
     const cart = await carts
-      .findOne({ user: user._id })
-      .toArray().sort({ datetime: -1});
+      .findOne({ userId: user._id });
 
-    
+    console.log(cart);
     res.send({ cart });
   } catch (err) {
     console.log(err);
